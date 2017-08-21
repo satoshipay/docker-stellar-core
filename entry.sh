a84a592626059aa83f6598c4ad335b66d31cf13a
Ishai Strauss <istrauss@koyadocutracker.com>
2017-08-21 16:39:13 -0500

    Automatically runs initdb on initial run.

--- entry.sh
+++ entry.sh
@@ -5,3 +5,16 @@ set -ue
 confd -onetime -backend env -log-level error
 
 exec "$@"
+
+init_stellar_core
+
+function init_stellar_core() {
+	if [ -f /.db-initialized ]; then
+		echo "core: already initialized"
+		return 0
+	fi
+
+	stellar-core --conf /stellar-core.cfg -newdb
+
+	touch /.db-initialized
+}
