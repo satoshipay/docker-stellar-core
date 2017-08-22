12603259eb0ccb248952fc0012f57d3d1789add3
Ishai Strauss <istrauss@koyadocutracker.com>
2017-08-21 20:15:38 -0500

    Adds some db initialization messages.

--- entry.sh
+++ entry.sh
@@ -10,11 +10,15 @@ stellar_core_init_db
 
 function stellar_core_init_db() {
 	if [ -f /.db-initialized ]; then
-		echo "core: already initialized"
+		echo "core db already initialized. continuing on..."
 		return 0
 	fi
 
+	echo "initializing core db..."
+
 	stellar-core --conf /stellar-core.cfg -newdb
+
+	echo "finished initializing core db"
 
 	touch /.db-initialized
 }
