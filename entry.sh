4b2a705fb95eb85c50069455d56ff5004249c694
Ishai Strauss <istrauss@koyadocutracker.com>
2017-08-22 21:57:29 -0500

    Adds first attempt at auto newhist.

--- entry.sh
+++ entry.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
+function stellar_core_newhist() {
+	if [ -f /data/.newhist-$1 ]; then
+		echo "history archive named $1 is already newed up."
+		return 0
+	fi
+
+	echo "newing up history archive: $1..."
+
+	stellar-core --newhist $1
+
+	echo "newing up history archive: $1"
+
+	touch /data/.newhist-$1
+}
+
 set -ue
 
 confd -onetime -backend env -log-level error
+
+jq -c 'keys[]' $HISTORY | while read archive_name; do
+    if [ jq '[$archive_name] | has("put")' ]; then
+        stellar_core_newhist $archive_name
+    fi
+done
 
 exec "$@"
