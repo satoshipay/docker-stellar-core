778eb58ae375769e406158f07aff5fa16d09b883
André Gaul <andre@gaul.io>
2017-08-23 08:16:34 +0200

    cleanup entry.sh

--- entry.sh
+++ entry.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-DB_INITIALIZED="/data/.db-initialized"
+set -ue
 
 function stellar_core_init_db() {
-	if [ -f $DB_INITIALIZED ]; then
-		echo "core db already initialized. continuing on..."
-		return 0
-	fi
+  local DB_INITIALIZED="/data/.db-initialized"
 
-	echo "initializing core db..."
+  if [ -f $DB_INITIALIZED ]; then
+    echo "core db already initialized. continuing on..."
+    return 0
+  fi
 
-	stellar-core --conf /stellar-core.cfg -newdb
+  echo "initializing core db..."
 
-	echo "finished initializing core db"
+  stellar-core --conf /stellar-core.cfg -newdb
 
-	touch $DB_INITIALIZED
+  echo "finished initializing core db"
+
+  touch $DB_INITIALIZED
 }
-
-set -ue
 
 confd -onetime -backend env -log-level error
 
