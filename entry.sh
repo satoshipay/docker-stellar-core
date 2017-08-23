64dd62a4ae5c1f1e8a972585700575f8b31ad2fa
Ishai Strauss <istrauss@koyadocutracker.com>
2017-08-22 20:55:22 -0500

    Adds jq to container.

--- entry.sh
+++ entry.sh
@@ -2,7 +2,7 @@
 
 DB_INITIALIZED="/data/.db-initialized"
 
-function stellar_core_init_db() {
+function stellar_core_newdb() {
 	if [ -f $DB_INITIALIZED ]; then
 		echo "core db already initialized. continuing on..."
 		return 0
@@ -17,10 +17,25 @@ function stellar_core_init_db() {
 	touch $DB_INITIALIZED
 }
 
+function stellar_core_newhist() {
+	if [ -f /data/.newhist-$1 ]; then
+		echo "history archive named $1 is already newed up."
+		return 0
+	fi
+
+	echo "newing up history archive: $1..."
+
+	stellar-core --newhist
+
+	echo "newing up history archive: $1"
+
+	touch /data/.newhist-$1
+}
+
 set -ue
 
 confd -onetime -backend env -log-level error
 
-stellar_core_init_db
+stellar_core_newdb
 
 exec "$@"
