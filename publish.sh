c7627d7dd46c40ed5019861b444bcc8302c5d0b4
André Gaul <andre@gaul.io>
2018-10-02 23:14:12 +0200

    add publish script

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ue
+
+if [ $# -ne 2 ]; then
+  echo "USAGE: publish.sh REPO TAG"
+  exit 1
+fi
+
+REPO=$1
+TAG=$2
+
+docker build -t ${REPO}:${TAG} .
+docker push ${REPO}:${TAG}
+
+docker build --build-arg TAG=${TAG} -t ${REPO}:${TAG}-aws -f Dockerfile.aws .
+docker push ${REPO}:${TAG}-aws
+
+docker build --build-arg TAG=${TAG} -t ${REPO}:${TAG}-gcloud -f Dockerfile.gcloud .
+docker push ${REPO}:${TAG}-gcloud
